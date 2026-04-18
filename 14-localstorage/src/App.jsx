f03e8ac0ed8935695ed54d9e47e8ceb7690b4a2b
XYZthedurgeshtiwari <tdurgesh448@gmail.com>
2026-04-18 11:07:06 +0530

    localStorage

---
+++ 14-localstorage/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const App = () => {
+
+  // localStorage.setItem('age',18)
+  // localStorage.getItem('user')
+  // localStorage.removeItem('user')
+
+  // const user = {
+  //   username:'Aman',
+  //   age:20,
+  //   city:'Kanpur'
+  // }
+
+  // localStorage.setItem('user',JSON.stringify(user))
+
+  // const user = JSON.parse(localStorage.getItem('user'))
+  // console.log(user)
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
